2eb6c3f117f3aede51746bd7649baa94e0c02dec
Trunggcd0808 <ttrung17072001@gmail.com>
2023-10-26 05:29:55 +0700

    back end

---
+++ Backend/DataImport.js
@@ -0,0 +1,19 @@
+import express from "express";
+import users from "./data/users.js";
+import User from "./Models/UserModel.js";
+import asyncHandler from "express-async-handler";
+
+const ImportData = express.Router();
+
+ImportData.post(
+    "/user",
+    asyncHandler(async (req, res) => {
+        await User.remove({});
+        const importUser = await User.insertMany(users);
+        res.send({ importUser });
+    })
+);
+
+
+
+export default ImportData;
